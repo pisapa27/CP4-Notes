0d70738867fb3afc065cf4f5e0fca6857fe496eb
pisapa27 <chazz.pisanelli19@alumni.loyola.ca>
2018-02-07 10:56:44 -0500

    Add files via upload

---
+++ Schedule.js
@@ -0,0 +1,41 @@
+var expr = ' Class name ';
+	switch (expr) {
+		case 'Math':
+			console.log('No Calculators');
+			break;
+		case 'English':
+			console.log('1 Coffee a Day');
+			break;
+		case 'History':
+			console.log('Notebooks Out');
+			break;
+		case 'French':
+			console.log('Français dans la classe');
+			break;
+		case 'Gym':
+			console.log('2 Laps around the Field');
+			break;
+		case 'Science':
+			console.log('Great Day for Science');
+			break;
+		case 'Theatre Arts':
+			console.log('Dollar or Jug');
+			break;
+		case 'Religion':
+			console.log('Vikings will Conquer');
+			break;
+		case 'Computer Programming':
+			console.log('Everything on Github');
+			break;
+		case 'Homeroom':
+			console.log('Pas de iPads');
+			break;
+		case 'Speech':
+			console.log('Prennez 5 minutes');
+			break;
+		case 'Assembly':
+			console.log('Everybody in your Seats');
+			break;
+	default:
+    console.log('Sorry,' + expr + 'is not a class');
+}
